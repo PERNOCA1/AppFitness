3dacf06e4fcf94c3d4e20d5f8a2d09fa083e99d9
Usuario <Usuario@CPD03-M13>
2022-11-08 12:20:41 -0300

    VIEW DE CADASTRO CONCEITOS MVVM

--- AppFitness/AppFitness/ViewModels/CadastroAtividadeViewModel.cs
+++ AppFitness/AppFitness/ViewModels/CadastroAtividadeViewModel.cs
@@ -1,10 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+using AppFitness.Models;
 
 namespace AppFitness.ViewModels
 {
-    internal class CadastroAtividadeViewModel
+    class CadastroAtividadeViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string descricao, observacoes;
+        int id;
+        DateTime data;
+        double? peso;
+
+        public string Descricao 
+        { 
+            get => descricao; 
+            set
+            {
+                descricao = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Descricao"));
+            }   
+        }
+        public string Observacoes 
+        { 
+            get => observacoes; 
+            set
+            {
+                observacoes = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Observacoes"));
+            } 
+        }
+        public int Id 
+        { 
+            get => id; 
+            set
+            {
+                id = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Id"));
+            } 
+        }
+        public DateTime Data 
+        { 
+            get => data; 
+            set
+            {
+                data = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Data"));
+            }
+        }
+        public double? Peso 
+        {
+            get => peso; 
+            set
+            {
+                peso = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Peso"));
+            } 
+        }
+
+
+        public ICommand NovaAtividade
+        {
+            get => new Command(() =>
+            {
+                Id = 0;
+                Descricao = String.Empty;
+                Data = DateTime.Now;
+                Peso = null;
+                Observacoes = String.Empty;
+
+            });
+        }
+
+        public ICommand SalvarAtividade
+        {
+            get => new Command(async () =>
+            {
+                try
+                {
+                    Atividade model = new Atividade()
+                    {
+                        Descricao = this.Descricao,
+                        Data = this.Data,
+                        Peso = this.Peso,
+                        Observacoes = this.Observacoes
+
+                    };
+
+                    if(this.Id == 0)
+                    {
+                        await App.Database.Insert(model);
+                    }
+                    else
+                    {
+                        model.Id = this.Id;
+
+                        await App.Database.Update(model);
+                    }
+
+                    await Application.Current.MainPage.DisplayAlert("Beleza!", "Atividade Salva!", "OK");
+
+                    await Shell.Current.GoToAsync("//MinhasAtividades");
+
+                } catch(Exception ex)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Ops", ex.Message, "OK");
+                }
+            });
+        }
+        
     }
 }
+
--- AppFitness/AppFitness/Views/CadastroAtividade.xaml.cs
+++ AppFitness/AppFitness/Views/CadastroAtividade.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using AppFitness.ViewModels;
+
 namespace AppFitness.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +17,19 @@ namespace AppFitness.Views
         public CadastroAtividade()
         {
             InitializeComponent();
+
+            BindingContext = new CadastroAtividadeViewModel();
+        }
+
+        protected override async void OnAppearing()
+        {
+            var vm = (CadastroAtividadeViewModel)BindingContext;
+
+            if(vm.Id == 0)
+            {
+                vm.NovaAtividade.Execute(null);
+            }
+            
         }
     }
 }
